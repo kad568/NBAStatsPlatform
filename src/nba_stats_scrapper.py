e3600ce4c70a7f1489d106a528a0a49f6bc00139
kad568 <kadmiel.m5@gmail.com>
2023-03-30 22:43:06 +0100

    completed league index method

--- src/nba_stats_scrapper.py
+++ src/nba_stats_scrapper.py
@@ -3,13 +3,13 @@ import scrapping_methods
 import sqlite3
 
 
-    
 def create_basketball_reference_database():
 
     basketball_reference_url = "https://www.basketball-reference.com/"
+
     league_index_url = f'{basketball_reference_url}/leagues'    
 
-    headers = [
+    league_index_headers = [
     "season", 
     "league", 
     "champion",
@@ -21,21 +21,27 @@ def create_basketball_reference_database():
     "win_share_leader"
     ]
 
+    
+
     with sqlite3.connect('basketball-reference.db') as connection:
             cursor = connection.cursor()
 
-            league_index_items, league_index_data  = scrapping_methods.get_table_data(league_index_url, headers)
-
+            league_index_items, league_index_data  = scrapping_methods.get_table_data(league_index_url, league_index_headers)
             utils.database.create_table(cursor, "league_index", league_index_items)
             utils.database.add_to_table(connection, cursor, "league_index", league_index_data)
+            del league_index_data, league_index_items
+
+
+
+
 
 
 def main():
 
-    # create_basketball_reference_database()
+    #create_basketball_reference_database()
     with sqlite3.connect('basketball-reference.db') as connection:
-        for _ in  utils.database.search(connection, 'league_index', 'points_leader'):
-            print(_)
+        for _ in  utils.database.search(connection, 'league_index', 'points_leader_link'):
+            print(_[0])
 
 
 if __name__ == "__main__":
--- src/scrapping_methods.py
+++ src/scrapping_methods.py
@@ -1,8 +1,34 @@
 import utils
 
+def clean_data(data, headers):
 
-def get_table_data(url: str,headers: list, proxies: dict = None) -> list: 
+    null_index = []
+
+    for header_index in range(len(data[0])):
+        header_data = [data_set[header_index] for data_set in data]
+
+        if all(data == "" for data in header_data):
+
+            null_index.append(header_index)
     
+    cleaned_data = []
+    for data_set in data:
+        cleaned_data_set = [item for index, item in enumerate(data_set) if not index in null_index]
+        cleaned_data.append(cleaned_data_set)
+
+    cleaned_headers = [header for index, header in enumerate(headers) if not index in null_index]
+
+    return (cleaned_headers, cleaned_data)
+
+
+def get_table_data(url: str,headers: list, proxies: dict = None, text_splitter = '\xa0') -> list: 
+
+    orig_headers = headers.copy()
+    for index, header in enumerate(orig_headers):
+         headers.insert(4 * index + 1, f"{header}_name")
+         headers.insert(4 * index + 2, f"{header}_link")
+         headers.insert(4 * index + 3, f"{header}_stat")
+
     data = []
 
     tags = utils.get_relevant_tags(url)
@@ -20,14 +46,34 @@ def get_table_data(url: str,headers: list, proxies: dict = None) -> list:
             else:
                  link = ""
 
+            if text_splitter in text:
+                name, stat = utils.split_player_stat(text)
+                text = ""
+            else:
+                name = ""
+                stat = ""
+            
             data_set.append(text)
+            data_set.append(name)
             data_set.append(link)
+            data_set.append(stat)
+        
+        data.append(data_set)
 
-        data.append(tuple(data_set))
-
-    # adjust original header
-    orig_headers = headers.copy()
-    for index, header in enumerate(orig_headers):
-         headers.insert(2 * index + 1, f"{header}_link")
+    headers, data = clean_data(data, headers)
 
     return (headers, data)
+
+def get_player(url: str,headers: list, proxies: dict = None) -> list:
+
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+    basketball_reference_url = "https://www.basketball-reference.com/" 
+    base_link = f'{basketball_reference_url}/players'   
+    player_links_by_letter = [f"{base_link}/{letter}/" for letter in alphabet]
+
+    
+    utils.get_relevant_tags(player_links_by_letter[0])
+
+    return
